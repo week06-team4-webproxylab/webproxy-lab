38b117816f29e430c3362063c8e280597c49d189
Jiho Kim <jiho001108@gmail.com>
2023-11-19 11:37:12 +0000

    231118 echoserver.c tiny.c 주석 추가

--- echo/echoserver.c
+++ echo/echoserver.c
@@ -5,23 +5,24 @@
 void echo(int connfd);
 
 int main(int argc, char **argv)
-{
-    int listenfd, connfd;
+{   
+    int listenfd, connfd; // 리스닝 소켓, 클라이언트 소켓
     socklen_t clientlen;
-    struct sockaddr_storage clientaddr; /* Enough space for any address */
+    struct sockaddr_storage clientaddr; // 클라이언트의 주소 정보 저장 /* Enough space for any address */
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
-    if (argc != 2)
+    if (argc != 2)  // argc 가 2가 아닐 경우 사용 방법을 출력하고 프로그램 종료
     {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
 
-    listenfd = Open_listenfd(argv[1]);
-    while (1)
+    listenfd = Open_listenfd(argv[1]); // 주어진 포트 번호에서 들어오는 연결 요쳥을 기다리는 리스닝 소켓을 생성
+    
+    while (1) // 무한 루프를 통해 서버는 연결 요청을 계속 기다린다.
     {
         clientlen = sizeof(struct sockaddr_storage);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 새로운 클라이언트의 연결 요청을 받아들이고, 해당 클라이언트와의 통신을 위한 새로운 소켓(connfd)을 생성
         Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE,
                     client_port, MAXLINE, 0);
         printf("Connected to (%s, %s)\n", client_hostname, client_port);
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -41,3 +41,90 @@ int main(int argc, char **argv) {
     Close(connfd);  // line:netp:tiny:close
   }
 }
+
+void doit(int fd)
+{
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+            // 버퍼(rio) 주소, 식별자fd와 연결
+  Rio_readinitb(&rio, fd);
+            //        
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request header:\n");
+  printf("%s", buf);  // 그대로 출력?
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET")) //?
+  {
+    clienterror(fd, method, "501", "Not implemented",
+                "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf) < 0)//?
+  {
+    clienterror(fd, filename, "404", "Not found",
+                  "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static)
+  { // 파일 종류 확인해서 읽기 권한을 확인하는 함수
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+    {                                 /* Read by owner.  */
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tiny couldn't read the file");
+      return;
+    }
+    serve_static(fd, filename, sbuf.st_size);
+  }
+  else
+  {                                   /* Execute by owner.  */
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode))
+    {
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tiny couldn't run the CGI program");
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs);
+  }
+}
+
+void clienterror(int fd, char *cause, char *errnum,
+char *shortmsg, char *longmsg)
+{
+char buf[MAXLINE], body[MAXBUF];
+/* Build the HTTP response body */
+sprintf(body, "<html><title>Tiny Error</title>");
+sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+/* Print the HTTP response */
+sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+Rio_writen(fd, buf, strlen(buf));
+sprintf(buf, "Content-type: text/html\r\n");
+Rio_writen(fd, buf, strlen(buf));
+sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+Rio_writen(fd, buf, strlen(buf));
+Rio_writen(fd, body, strlen(body));
+}
+
+void read_requesthdrs(rio_t *rp)
+{
+  char buf[MAXLINE];
+
+  Rio_readlineb(rp, buf, MAXLINE);
+  while (strcmp(buf, "\r\n")) // 대소문자 구분없이 buf 읽는다
+  {
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+  }
+  return;
+}
+
