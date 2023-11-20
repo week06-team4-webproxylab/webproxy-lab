02d61e474e6b926a5c6aa9bda856dc27b9c8b253
Jiho Kim <jiho001108@gmail.com>
2023-11-20 13:21:30 +0000

    숙제 문제 11.6C, 11.7 해결

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -205,6 +205,10 @@ void get_filetype(char *filename, char *filetype)
     strcpy(filetype, "image/png");
   else if (strstr(filename, ".jpg"))
     strcpy(filetype, "image/jpeg");
+  else if (strstr(filename, ".mpg"))
+    strcpy(filetype, "video/mpg");
+  else if (strstr(filename, ".mp4"))
+    strcpy(filetype, "video/mp4");
   else
     strcpy(filetype, "text/plain");
 }
