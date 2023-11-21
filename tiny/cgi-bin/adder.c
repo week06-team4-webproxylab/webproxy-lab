f222b96b6e455ea60c373bbb4dbdf108d0e88bb3
Jiho Kim <jiho001108@gmail.com>
2023-11-21 07:16:19 +0000

    adder.c 추가 서버 작동 안함

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,40 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void) 
+{
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  { // buf에 쿼리 스트링 들어가있다.
+    printf("adder.c의 buf: %s\n", buf);
+    p = strchr(buf, '&');
+    *p = '\0'; // `&`를 NULL로 바꿈
+    strcpy(arg1, buf);
+    strcpy(arg2, p+1);
+    n1 = atoi(arg1);
+    n2 = atoi(arg2);
+  }
+
+  /* Make the response body */
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+  
+  /* Generate the HTTP response */
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
+  
   exit(0);
-}
+} 
+
+  
 /* $end adder */
